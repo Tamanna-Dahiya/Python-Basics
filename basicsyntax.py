8debe63c5dc3b25f8ad6e394af262745fba4c476
Tamanna-Dahiya <tamannadahiya726@gmail.com>
2024-09-18 21:38:07 +0530

    basic syntax
    for revision of conditional statements

---
+++ basicsyntax.py
@@ -0,0 +1,38 @@
+# conditional statements
+# syntax of basic if else statement
+a=6
+if a<6:
+    print("true")
+else:
+    print("false")
+# syntax of if elif else statement
+a=7
+if a<5:
+    print("true")
+elif a==7:
+    print("given number")
+else :
+    print("false") 
+# syntax for giving multiple if statements( will execute independently of each other)
+a=8
+if a<4:
+    print("false")
+if a>4:
+    print("true")
+if a is 8:# is performs here same operation as of == 
+    print("same ")
+elif a<6:
+    print("x")
+else:
+    print("no") 
+# basic syntax of multiple elif statements
+a=7
+if a<6:
+    print("np")
+elif a ==4:
+    print("false")
+elif a==7:
+    print("yes")
+else:
+    pass
+# done                                               
