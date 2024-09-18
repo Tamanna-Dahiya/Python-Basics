65ebb0457d7c951f51174a8167594992133caa26
Tamanna-Dahiya <tamannadahiya726@gmail.com>
2024-09-18 21:48:41 +0530

    loops
     for loop,while loop and basic statements-pass,break,continue

---
+++ break,continue,pass.py
@@ -0,0 +1,11 @@
+#use of break statement in python
+ #for i in range(6):
+    #print(i)
+    #if i==4:
+        #break
+# use of continue statement
+for i in range (9):
+    
+    if i==3:
+        continue
+    print(i)   
---
+++ loops.py
@@ -0,0 +1,24 @@
+#using while loop
+#program to print number from 1 to 100
+i=1
+while i<=100:
+    print(i)
+    i=i+1
+#program to print yes 5 times
+i=1
+while i<=5:
+    print("yes")
+    i=i+1
+#program to print table of 5
+i=1
+while i <=10:
+    print(5*i)
+    i=i+1
+#program to print all contents of list
+l=["apple","banana","mango","orange"]
+while i <len(l):
+    print(l[i])
+    i=i+1
+            
+    
+
