5dde7fa0c80d83f57f1749f701a9a2d091d5e34d
Tamanna-Dahiya <tamannadahiya726@gmail.com>
2024-09-18 21:45:51 +0530

    practice questions
     excercise questions on llist,tuples,dictionary,set

---
+++ chapter5practiceset.py
@@ -0,0 +1,30 @@
+#program to create a dictionary of hindi words with values as their english translation
+ #d={"ham":"we","chale joa":"go","bhasa":"language"}
+ #print(d)
+#program to input 8 number from user and then print unique numbers
+ #a=int(input("enter first number:"))
+ #b=int(input("enter second number:"))
+ #c=int(input("enter third number:"))
+ #d=int(input("enter fourth number:"))
+ #e=int(input("enter fifth number:"))
+ #f=int(input("enter sixth number;"))
+ #g=int(input("enter seventrh number:"))
+ #h=int(input("enter eighth number:"))
+ #s={a,b,c,d,e,f,g,h}
+ #print(s)
+#program to check whether a integer or string can be a element of set
+ #s={18,"18"}
+ #print(s)
+#program to check length of setpy
+ #s=set()
+ #s.add(20)
+#s.add(20.0)
+#s.add("20")
+#print(len(s))
+ #program to create an empty dictionary and enter your friend's favourite language as values and their names as keys
+d={}
+d["a"]="japanese"
+d["b"]="japanese"
+d["c"]="english"
+d["d"]="hindi"
+print(d)
